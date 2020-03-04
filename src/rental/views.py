e64b4678485e0763c88dc4e805d8b3718e1c5783
Chutchai Suebklang <chutchai.s@gmail.com>
2020-03-04 17:32:08 +0700

    Change function to support Progresql

--- src/rental/views.py
+++ src/rental/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.db.models import Q
-from django.db.models import (F,Min,Max,Avg,StdDev,Count,Sum,
+from django.db.models import (ExpressionWrapper,F,Min,Max,Avg,StdDev,Count,Sum,
 							Value, When,Case,IntegerField,CharField,FloatField)
 from django.db.models.functions import Cast
 from django.db.models.fields import DateField
 from django.db.models.functions import Floor,Ceil
+from django.db.models.functions import Mod
 
 # Create your views here.
 from django.http import JsonResponse
@@ -25,7 +26,7 @@ def get_rental(terminal,str_start_date,str_stop_date,detail):
 	# if need details 
 	if detail == 'yes':
 		return  rentel_list
-		
+
 	# Default is summary by CHE Kind
 	kinds = Kind.objects.all().order_by('name')
 	kind_list = list(kinds.values('name','title'))
@@ -52,6 +53,7 @@ def get_kind_total_hour(rental_list,kind):
 
 
 def get_rental_detail(terminal,str_start_date,str_stop_date):
+	import math
 	from datetime import datetime, timedelta
 	start_date 			= None
 	stop_date 			= None
@@ -64,20 +66,46 @@ def get_rental_detail(terminal,str_start_date,str_stop_date):
 		rentals = Rental.objects.filter(terminal=terminal,
 									rent_date__range=[start_date,stop_date])
 		reports 	= rentals.values('rent_date__date','che__kind').annotate(
-							total_containers = Count('container'),
-							total_handling_time = Sum('che__kind__handling_time'),
-							transportation_time = Max('che__kind__trans_time'),
-							total_time =F('total_handling_time')+F('transportation_time'),
-							mod_minute = F('total_time')%60,
-							total_hours = Floor(F('total_time')/60) + 
-								Case(When(mod_minute__gt =  30 , then=Value(1)),
-									When(mod_minute__range =  [1,29] , then=Value(0.5)),
-									default = Value(0),
-	        						output_field = FloatField(),)
-							).order_by('rent_date__date','che__kind')
+								total_containers = Count('container'),
+								total_handling_time = Sum('che__kind__handling_time'),
+								transportation_time = Max('che__kind__trans_time'),
+								total_minutes =F('total_handling_time')+F('transportation_time'),
+								mod_minute = Floor(F('total_minutes')/60),
+								total_hours = Floor(F('total_minutes')/60)).order_by('rent_date__date','che__kind')
 		
+		for r in reports:
+			mod_minute = r['total_minutes'] % 60
+			hours 	= math.floor(r['total_minutes'] / 60)
+			hours 	= hours + (0.5 if mod_minute > 0 and mod_minute < 30 else 0)
+			hours 	= hours + (1 if mod_minute >= 30 else 0)
+			r['mod_minute'] = mod_minute
+			r['total_hours'] = hours
+			# print(r['total_minutes'], hours,mod_minute)
 
-		# containers = rentals.values('terminal','che','container','rent_date')
 		container_list = list(reports)
 
 	return container_list
+
+
+	# reports 	= rentals.values('rent_date__date','che__kind').annotate(
+	# 						total_containers = Count('container'),
+	# 						total_handling_time = Sum('che__kind__handling_time'),
+	# 						transportation_time = Max('che__kind__trans_time'),
+	# 						total_time =F('total_handling_time')+F('transportation_time'),
+	# 						mod_minute = Mod(F('total_time'),60),
+	# 						total_hours = Floor(F('total_time')/60) + 
+	# 							Case(When(mod_minute__gt =  30 , then=Value(1)),
+	# 								When(mod_minute__range =  [1,29] , then=Value(0.5)),
+	# 								default = Value(0),
+	#         						output_field = FloatField())
+	# 						).order_by('rent_date__date','che__kind')
+
+
+		# reports 	= rentals.values('rent_date__date','che__kind').annotate(
+		# 						total_containers = Count('container'),
+		# 						total_handling_time = Sum('che__kind__handling_time'),
+		# 						transportation_time = Max('che__kind__trans_time'),
+		# 						total_time = ExpressionWrapper(F('total_handling_time')+F('transportation_time'),output_field=IntegerField()),
+		# 						mod_minute = ExpressionWrapper(Mod(62,60),output_field=IntegerField()),
+		# 						total_hours = F('total_handling_time')/60).order_by(
+		# 						'rent_date__date','che__kind')
